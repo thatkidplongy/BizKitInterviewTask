5ca496db4f0d2db416ccb5d8baf786e103bca539
Florante Clavano Jr <fgclavano@gmail.com>
2022-10-17 16:45:29 +0800

    BizKit Interview Challenge

--- phasebook/match.py
+++ phasebook/match.py
@@ -20,8 +20,9 @@ def match(match_id):
 
 
 def is_match(fave_numbers_1, fave_numbers_2):
-    for number in fave_numbers_2:
-        if number not in fave_numbers_1:
-            return False
-
-    return True
+    new_fave_numbers_1 = set(fave_numbers_1)
+    new_fave_numbers_2 = set(fave_numbers_2)
+    if(new_fave_numbers_2.issubset(new_fave_numbers_1)):
+        return True
+    
+    return False
--- phasebook/search.py
+++ phasebook/search.py
@@ -26,5 +26,38 @@ def search_users(args):
     """
 
     # Implement search here!
+    if not args:
+        return USERS
+    
+    else:
+        filters = []
+        print(args)
+        id = args.get('id')
+        name = args.get('name')
+        age = args.get('age')
+        occupation = args.get('occupation')
+         
+        for user in USERS:
 
-    return USERS
+            if id:
+                if args['id'] == user['id']:
+                    filters.append(user)
+            
+            if name:
+                if args['name'].lower() in user['name'].lower():
+                    filters.append(user)
+            
+            if age:
+                age = int(age)
+                if user['age'] in range((age-1), (age+2)):
+                    filters.append(user)
+            
+            if occupation:
+                if args['occupation'].lower() in user['occupation'].lower():
+                    filters.append(user)
+
+        filters = [dict(tuple) for tuple in {tuple(filter.items()) for filter in filters}]
+
+        filters = sorted(filters, key=lambda filter: (filter['id'], filter['name'], filter['age'], filter['occupation']))
+
+        return filters
